da1e89035ad552313a3797a0eeadeee6a3f52b57
shapker <gzhhsgxgxv@gmail.com>
2025-10-16 23:59:07 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'shapker-bot-v1';
+const CACHE_NAME = 'shapker-bot-v2.0';
 const urlsToCache = [
     '/',
     '/index.html',
@@ -9,6 +9,7 @@ self.addEventListener('install', function(event) {
     event.waitUntil(
         caches.open(CACHE_NAME)
             .then(function(cache) {
+                console.log('Opened cache');
                 return cache.addAll(urlsToCache);
             })
     );
@@ -24,5 +25,20 @@ self.addEventListener('fetch', function(event) {
                 return fetch(event.request);
             }
         )
+    );
+});
+
+self.addEventListener('activate', function(event) {
+    event.waitUntil(
+        caches.keys().then(function(cacheNames) {
+            return Promise.all(
+                cacheNames.map(function(cacheName) {
+                    if (cacheName !== CACHE_NAME) {
+                        console.log('Deleting old cache:', cacheName);
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
     );
 });
